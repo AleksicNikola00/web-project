8ef4011ee273fb79c6bc3fd924460272b08517a8
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-05-23 23:58:04 +0200

    zapocet dashboard

---
+++ WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
@@ -0,0 +1,17 @@
+var webShop = new Vue({
+    el: '#dashboard',
+    data: {
+        products : [
+            {name: 'pera'},
+            {name: 'mika'},
+            {name: 'zika'},
+            {name: 'zare'},
+            {name: 'afs'},
+            {name: 'fsfs'},
+            {name: 'pesfsfra'}
+        ]
+    },
+    mounted (){
+
+    }
+})
