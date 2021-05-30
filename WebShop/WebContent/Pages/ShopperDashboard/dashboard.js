620c0779cdab699ae1a3caa954994369d9b46fdc
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-05-30 13:19:12 +0200

    zapocet dashboard

--- WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
+++ WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
@@ -9,9 +9,24 @@ var webShop = new Vue({
             {name: 'afs'},
             {name: 'fsfs'},
             {name: 'pesfsfra'}
-        ]
+        ],
+        selectedButton : {},
+        currentUser : {},
+        visible : 'myProfile'
     },
     mounted (){
-
+        this.products.push({name: 'lol'});
+    },
+    methods : {
+        selectSubmenu : function(submenu){
+            if (Object.keys(this.selectedButton).length != 0){
+                this.selectedButton.removeClass('btn-primary');
+                this.selectedButton.addClass('btn-light');
+            }
+            this.selectedButton = $('#' + submenu);
+            this.selectedButton.removeClass('btn-light');
+            this.selectedButton.addClass('btn-primary');
+            this.visible = submenu;
+        }
     }
 })
