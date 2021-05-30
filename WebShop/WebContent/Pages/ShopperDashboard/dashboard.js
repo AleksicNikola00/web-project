d2f8495102b514837bd838a9d2c87f428b1abdcd
Nikola Milosavljević <milosavljevic.ra5.2018@uns.ac.rs>
2021-05-31 00:08:09 +0200

    odradjen prikaz restorana i prikaz podataka

--- WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
+++ WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
@@ -7,83 +7,95 @@ var webShop = new Vue({
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'true'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'false'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'false'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'true'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'true'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'false'
             },
             {
                 name : 'Ciao',
                 type : 'Italian',
                 rating : '4.7*',
                 address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
-            },
-            {
-                name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
-            },
-            {
-                name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
-            },
-            {
-                name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
-            },{
-                name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
-                city : 'Novi Sad'
+                city : 'Novi Sad',
+                open : 'true'
             }
         ],
         selectedButton : {},
-        currentUser : {},
-        visible : 'myProfile'
+        currentUser : {
+            username : 'mili',
+            firstname : 'Nikola',
+            lastname : 'Milosavljevic',
+            email : 'nikolamilosa20@gmail.com',
+            dateOfBirth: '4-7-1999',
+            points : '1000'
+        },
+        tempCurrUser : {},
+        visible : 'restaurants',
+        status : {},
+        filterObj : {
+            name : '',
+            location : '',
+            type : '',
+            rating : 1,
+            isOpen : false,
+            isAsc : true,
+            isDesc : false
+        }
+    },
+    created (){
     },
     mounted (){
+        this.selectSubmenu(this.visible);
+
+        this.tempCurrUser = Object.assign({}, this.currentUser);
+        this.tempCurrUser.dateOfBirth = this.convertDate(this.tempCurrUser.dateOfBirth);
+        this.tempCurrUser.points = parseInt(this.tempCurrUser.points);
+
+        if (this.tempCurrUser.points < 500)
+            this.status = 'bronze';
+        else if (this.tempCurrUser.points < 1000)
+            this.status = 'silver';
+        else
+            this.status = 'gold';
     },
     methods : {
         selectSubmenu : function(submenu){
@@ -95,6 +107,31 @@ var webShop = new Vue({
             this.selectedButton.removeClass('btn-light');
             this.selectedButton.addClass('btn-primary');
             this.visible = submenu;
+        },
+        convertDate : function(date) {
+            parts = date.split('-');
+
+            day = parseInt(parts[0]);
+            
+            if (day < 10){
+                day = "0" + day.toString();
+            }
+            else{
+                day = day.toString();
+            }
+
+            month = parseInt(parts[1]);
+            
+            if (month < 10){
+                month = "0" + month.toString();
+            }
+            else{
+                month = month.toString();
+            }
+
+            d = parts[2] + "-" + month + "-" + day;
+
+            return d;
         }
     }
 })
