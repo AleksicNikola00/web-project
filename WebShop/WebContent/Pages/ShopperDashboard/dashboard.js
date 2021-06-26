a418676e7b7ec467ec1de2928bcfa26c88da9917
NikolaRA5 <milosavljevic.ra5.2018@uns.ac.rs>
2021-06-26 12:53:08 +0200

    zavrseno filtriranje na glavnom dashbordu

--- WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
+++ WebShop/WebContent/Pages/ShopperDashboard/dashboard.js
@@ -6,33 +6,33 @@ var webShop = new Vue({
         restaurants : [],
         receivedRestaurants : [
             {
-                name : 'Ciao',
+                name : 'Pizza',
                 type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
+                rating : '2',
+                address : 'Test a',
                 city : 'Novi Sad',
                 open : 'true'
             },
             {
                 name : 'Neki',
-                type : 'Italian',
-                rating : '4.7*',
-                address : 'Bulevar oslobodjenja',
+                type : 'Turkish',
+                rating : '3',
+                address : 'Test b',
                 city : 'Novi Sad',
                 open : 'false'
             },
             {
-                name : 'Drugi',
-                type : 'Italian',
-                rating : '4.7*',
+                name : 'Naki',
+                type : 'Turkish',
+                rating : '3',
                 address : 'Bulevar oslobodjenja',
                 city : 'Novi Sad',
                 open : 'false'
             },
             {
                 name : 'Opet',
-                type : 'Italian',
-                rating : '4.7*',
+                type : 'Cheenese',
+                rating : '2',
                 address : 'Bulevar oslobodjenja',
                 city : 'Novi Sad',
                 open : 'true'
@@ -40,23 +40,23 @@ var webShop = new Vue({
             {
                 name : 'Ciao',
                 type : 'Italian',
-                rating : '4.7*',
+                rating : '5',
                 address : 'Bulevar oslobodjenja',
                 city : 'Novi Sad',
                 open : 'true'
             },
             {
                 name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
+                type : 'Itelian',
+                rating : '1',
                 address : 'Bulevar oslobodjenja',
                 city : 'Novi Sad',
                 open : 'false'
             },
             {
                 name : 'Ciao',
-                type : 'Italian',
-                rating : '4.7*',
+                type : 'Cheenese',
+                rating : '4',
                 address : 'Bulevar oslobodjenja',
                 city : 'Novi Sad',
                 open : 'true'
@@ -69,7 +69,7 @@ var webShop = new Vue({
             lastname : 'Milosavljevic',
             email : 'nikolamilosa20@gmail.com',
             dateOfBirth: '4-7-1999',
-            points : '1000'
+            points : '600'
         },
         tempCurrUser : {},
         visible : 'restaurants',
@@ -78,11 +78,12 @@ var webShop = new Vue({
             name : '',
             location : '',
             type : '',
-            rating : 1,
+            rating : 'all',
             isOpen : false,
             isAsc : true,
             isDesc : false
-        }
+        },
+        selectedRestestaurant : {}
     },
     created (){
     },
@@ -93,15 +94,18 @@ var webShop = new Vue({
         this.tempCurrUser = Object.assign({}, this.currentUser);
         this.tempCurrUser.dateOfBirth = this.convertDate(this.tempCurrUser.dateOfBirth);
         this.tempCurrUser.points = parseInt(this.tempCurrUser.points);
-
-        if (this.tempCurrUser.points < 500)
-            this.status = 'bronze';
-        else if (this.tempCurrUser.points < 1000)
-            this.status = 'silver';
-        else
-            this.status = 'gold';
+        this.selectStatus();
+        
     },
     methods : {
+        selectStatus : function() {
+            if (this.tempCurrUser.points < 500)
+            this.status = 'bronze';
+            else if (this.tempCurrUser.points < 1000)
+                this.status = 'silver';
+            else
+                this.status = 'gold';
+        },
         selectSubmenu : function(submenu){
             actualSubmenu = submenu;
             if (submenu == 'specificRestaurant')
@@ -119,6 +123,17 @@ var webShop = new Vue({
             this.selectedButton.addClass('btn-primary');
             this.visible = submenu;
             return;
+        },
+        descClick : function(){
+            this.filterObj.isAsc = false;
+            this.filterObj.isDesc = true;
+        },
+        ascClick : function(){
+            this.filterObj.isDesc = false;
+            this.filterObj.isAsc = true;
+        },
+        isOpenClick : function(){
+            this.filterObj.isOpen = !this.filterObj.isOpen;
         },
         convertDate : function(date) {
             parts = date.split('-');
@@ -145,19 +160,125 @@ var webShop = new Vue({
 
             return d;
         },
-        filterName : function() {
-            this.restaurants = this.receivedRestaurants.filter(rest => rest.name.toLowerCase().includes(this.filterObj.name.toLowerCase()));
-            return;
+        filterName : function(restaurants) {
+            let result;
+            result = restaurants.filter(rest => rest.name.toLowerCase().includes(this.filterObj.name.toLowerCase()));
+            result.sort(function(a, b){
+                return ('' + a.name).localeCompare(b.name);
+            });
+
+            if (this.filterObj.isDesc)
+            {
+                result.reverse();
+            }
+            return result;
+        },
+        filterType : function(restaurants) {
+            let result;
+            result = restaurants.filter(rest => rest.type.toLowerCase().includes(this.filterObj.type.toLowerCase()));
+            result.sort(function(a, b){
+                return ('' + a.type).localeCompare(b.type);
+            });
+
+            if (this.filterObj.isDesc)
+            {
+                result.reverse();
+            }
+            return result;
+        },
+        filterLocation : function(restaurants){
+            let result;
+            result = restaurants.filter(rest => rest.address.toLowerCase().includes(this.filterObj.location.toLowerCase()));
+            result.sort(function(a, b){
+                return ('' + a.location).localeCompare(b.location);
+            });
+
+            if (this.filterObj.isDesc){
+                result.reverse();
+            }
+
+            return result;
+        },
+        filterRating : function(restaurants){
+            let result;
+            result = restaurants.filter(rest => parseFloat(rest.rating) >= parseFloat(this.filterObj.rating));
+            result.sort(function(a, b){
+                return a.rating - b.rating;
+            });
+
+            if (this.filterObj.isDesc){
+                result.reverse();
+            }
+
+            return result;
+        },
+        filterIsOpen : function(restaurants){
+            let result;
+            result = restaurants.filter(rest => rest.open == 'true');
+
+            return result;
+        },
+        doFilter : function(){
+            let result = this.receivedRestaurants;
+            
+            
+            if (this.filterObj.rating != 'all'){
+                result = this.filterRating(result);
+            }
+            if (this.filterObj.isOpen){
+                result = this.filterIsOpen(result);
+            }
+            if (this.filterObj.type != ''){
+                result = this.filterType(result);
+            }
+            if (this.filterObj.location != ''){
+                result = this.filterLocation(result);
+            }
+            if (this.filterObj.name != ''){
+                result = this.filterName(result);
+            }
+
+            this.restaurants = result;
+        },
+
+        pressedRestaurant : function(restaurant){
+            
         }
     },
     computed: {
+        /* used for filtering */
         filterObjName() {
             return this.filterObj.name;
+        },
+        filterObjType() {
+            return this.filterObj.type;
+        },
+        filterObjLocation() {
+            return this.filterObj.location;
+        },
+        filterObjRaiting() {
+            return this.filterObj.rating;
+        },
+        filterObjIsOpen() {
+            return this.filterObj.isOpen;
         }
     },
     watch: {
+        /* used for filtering */
         filterObjName() {
-            this.filterName();
+            this.doFilter();
+        },
+        filterObjType() {
+            this.doFilter();
+        },
+        filterObjLocation() {
+            this.doFilter();
+        },
+        filterObjRaiting() {
+            this.doFilter();
+        },
+        filterObjIsOpen() {
+            this.doFilter();
         }
     }
 })
