15930b7c0ce5fccaf710b1416da4720486ab3019
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-07-01 23:33:10 +0200

    implementirana logika izbora tab-a  unutar homepage-a

---
+++ WebShop/WebContent/Pages/DeliveryBoyDashboard/dashboard.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+	el: '#dashboard',
+	data: {
+        selectedButton : '',
+        activeSubmenu : ''
+	},
+	mounted() {
+		this.selectedButton = 'restaurants';
+        this.changeVisibility();
+	},
+	methods: {
+		selectSubmenu: function(submenu){
+			var elem = $('#' + this.selectedButton);
+            elem.removeClass('btn-primary');
+            elem.addClass('btn-light');
+            //na novog
+            this.selectedButton = submenu
+            var elem = $('#' + this.selectedButton);
+            elem.removeClass('btn-light');
+            elem.addClass('btn-primary');
+            this.changeVisibility();
+		},
+        changeVisibility: function(){
+            this.activeSubmenu = this.selectedButton;
+        }
+
+	}
+});
