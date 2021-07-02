5ef7d7ee352595f863828eda03cf0ab0d995245e
AleksicNikola00 <73480578+AleksicNikola00@users.noreply.github.com>
2021-07-02 19:56:56 +0200

    dodata logika za otvorene i zatvorene restorane

--- WebShop/WebContent/Pages/DeliveryBoyDashboard/dashboard.js
+++ WebShop/WebContent/Pages/DeliveryBoyDashboard/dashboard.js
@@ -3,7 +3,64 @@ var app = new Vue({
 	data: {
         selectedButton : '',
         activeSubmenu : '',
-        restaurants : {}
+        restaurants : [
+            {
+                name : 'Pizza',
+                type : 'Italian',
+                rating : '2',
+                address : 'Test a',
+                city : 'Novi Sad',
+                open : 'true'
+            },
+            {
+                name : 'Neki',
+                type : 'Turkish',
+                rating : '3',
+                address : 'Test b',
+                city : 'Novi Sad',
+                open : 'false'
+            },
+            {
+                name : 'Naki',
+                type : 'Turkish',
+                rating : '3',
+                address : 'Bulevar oslobodjenja',
+                city : 'Novi Sad',
+                open : 'false'
+            },
+            {
+                name : 'Opet',
+                type : 'Cheenese',
+                rating : '2',
+                address : 'Bulevar oslobodjenja',
+                city : 'Novi Sad',
+                open : 'true'
+            },
+            {
+                name : 'Ciao',
+                type : 'Italian',
+                rating : '5',
+                address : 'Bulevar oslobodjenja',
+                city : 'Novi Sad',
+                open : 'true'
+            },
+            {
+                name : 'Ciao',
+                type : 'Itelian',
+                rating : '1',
+                address : 'Bulevar oslobodjenja',
+                city : 'Novi Sad',
+                open : 'false'
+            },
+            {
+                name : 'Ciao',
+                type : 'Cheenese',
+                rating : '4',
+                address : 'Bulevar oslobodjenja',
+                city : 'Novi Sad',
+                open : 'true'
+            }
+        ],
 	},
 	mounted() {
 		this.selectedButton = 'restaurants';
